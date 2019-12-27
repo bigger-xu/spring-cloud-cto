1c2767467f9a2f12ac5b556e0c7f6e8ca4e1cf1c
Mr.Zhang <562123314@qq.com>
2019-12-27 23:39:50 +0800

    修改网关配置

--- cto-oauth/src/main/java/com.cto.cloud/AuthApplication.java
+++ cto-oauth/src/main/java/com.cto.cloud/AuthApplication.java
@@ -1,14 +1,16 @@
 package com.cto.cloud;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author Zhang Yongwei
  * @version 1.0
  * @date 2019-12-15
  */
-@SpringCloudApplication
+@EnableEurekaClient
+@SpringBootApplication
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class,args);
